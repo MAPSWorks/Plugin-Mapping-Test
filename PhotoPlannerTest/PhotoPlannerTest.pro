db3d04dd9751c98dea666f0320163cd5117555cd
anb1971 <bogomolov@tut.by>
2017-07-12 22:14:49 +0300

    Added small example with Proj.4 library

--- PhotoPlannerTest/PhotoPlannerTest.pro
+++ PhotoPlannerTest/PhotoPlannerTest.pro
@@ -20,3 +20,8 @@ else:win32-g++:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../Phot
 else:win32:!win32-g++:CONFIG(release, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../PhotoPlanner/release/PhotoPlanner.lib
 else:win32:!win32-g++:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../PhotoPlanner/debug/PhotoPlanner.lib
 else:unix: PRE_TARGETDEPS += $$OUT_PWD/../PhotoPlanner/libPhotoPlanner.a
+
+
+
+unix: CONFIG += link_pkgconfig
+unix: PKGCONFIG += proj
--- PhotoPlannerTest/tst_photoplanner.cpp
+++ PhotoPlannerTest/tst_photoplanner.cpp
@@ -2,7 +2,10 @@
 
 // add necessary includes here
 
+#include "photogeocalcs.h"
 #include "photoprintsgenerator.h"
+
+#include <proj_api.h>
 
 class PhotoPlanner : public QObject
 {
@@ -15,7 +18,53 @@ public:
 private slots:
     void initTestCase();
     void cleanupTestCase();
-    void test_case1();
+
+    void test_basic_math() {
+        QCOMPARE(sqrt(25), double(5));
+    }
+
+    void test_proj4() {
+        projPJ pj_merc = pj_init_plus("+proj=merc +ellps=clrk66 +lat_ts=33");
+        QVERIFY(pj_merc!=nullptr);
+        projPJ pj_latlong = pj_init_plus("+proj=latlong +ellps=clrk66");
+        QVERIFY(pj_latlong!=nullptr);
+        double x = -16; x *= DEG_TO_RAD;
+        double y = 20.25; y*= DEG_TO_RAD;
+        auto errcode = pj_transform(pj_latlong, pj_merc, 1, 1, &x, &y, NULL );
+        QCOMPARE (errcode, 0);
+        // qDebug("x='%f' y='%f'", x, y);
+        QCOMPARE (x, -1495284.211473);
+        QCOMPARE (y, 1920596.789917);
+        pj_free(pj_latlong);
+        pj_free(pj_merc);
+    }
+
+    void test_geo_calcs() {
+//        using namespace aero_photo;
+//        const double distance = 5000;
+//        const double azimuth = 0;
+//        RunStartPointsCalc calc(GeoPoint(0, 0), azimuth, distance, 2);
+//        GeoPoints starts = { calc.Calculate(0), calc.Calculate(1) };
+//        for (double offset = 0; offset < 2e7; offset += 1e6) {
+//            qDebug("Checked offset %f %f", offset, starts[0].atDistanceAndAzimuth(offset, azimuth).distanceTo(starts[1].atDistanceAndAzimuth(offset, azimuth)));
+//        }
+    }
+
+    void test_case1() {
+        //    using namespace aero_photo;
+        //    GeoPoint startPoint(47.2589912414551, 11.3327512741089);
+        //    GeoPoints testAreaBorder;
+        //    testAreaBorder << startPoint;
+        //    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(1100, 90);
+        //    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(220, 180);
+        //    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(1100, -90);
+        //    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(220, 0);
+        //    QVERIFY (testAreaBorder.front().distanceTo(testAreaBorder.back()) < 1);
+        //    TargetArea testArea(testAreaBorder);
+        //    LinearPhotoPrintsGenerator generator(testArea);
+        //    auto photoPrints = generator.GeneratePhotoPrints(200, 50);
+        //    QCOMPARE(photoPrints.size(), 16);
+    }
 
 };
 
@@ -37,23 +86,6 @@ void PhotoPlanner::initTestCase()
 void PhotoPlanner::cleanupTestCase()
 {
 
-}
-
-void PhotoPlanner::test_case1()
-{
-    using namespace aero_photo;
-    GeoPoint startPoint(47.2589912414551, 11.3327512741089);
-    GeoPoints testAreaBorder;
-    testAreaBorder << startPoint;
-    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(1100, 90);
-    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(220, 180);
-    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(1100, -90);
-    testAreaBorder << testAreaBorder.back().atDistanceAndAzimuth(220, 0);
-    QVERIFY (testAreaBorder.front().distanceTo(testAreaBorder.back()) < 1);
-    TargetArea testArea(testAreaBorder);
-    LinePhotoPrintsGenerator generator(testArea);
-    auto photoPrints = generator.GeneratePhotoPrints(200, 50);
-    QCOMPARE(photoPrints.size(), 16);
 }
 
 QTEST_APPLESS_MAIN(PhotoPlanner)
